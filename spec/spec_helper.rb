3652701a2cdba4cc3d9b2f30a7e392f9b7b4a567
kaspernj <kaspernj@gmail.com>
2020-12-10 15:42:42 +0100

    Fixed Lint/EmptyBlock offence

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,3 @@ require "html_gen"
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].sort.each { |f| require f }
-
-RSpec.configure do |config|
-end
